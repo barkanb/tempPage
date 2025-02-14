1a75a47acd4410536b9b64e4507595d9e086c248
Boria Barkan <42649953+barkanb@users.noreply.github.com>
2025-02-14 01:26:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const message = "Dragonroom address will be provided Saturday morning at 1 AM.";
 
     function displayAddress() {
-       ();
+        const now = new Date();
         const hours = now.getUTCHours() - 6; // Convert UTC to Central Time
         const day = now.getUTCDay(); // Get the day of the week (0 = Sunday, 6 = Saturday)
 
