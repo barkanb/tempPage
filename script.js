b8e13574fa3fa54393eb02520240c0d65377377d
Boria Barkan <42649953+barkanb@users.noreply.github.com>
2025-02-15 01:08:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const addressDiv = document.getElementById("address");
-    const countdownDiv = document.getElementById("countdown");
-    const address = "2239 S Michigan Ave, Chicago, IL 60616";
-    const message = "Dragonroom address will be provided Saturday morning at 1 AM.";
 
-  
-    setInterval(displayAddress, 60000); // Check every minute
-    setInterval(updateCountdown, 1000); // Update countdown every second
+    const address = "2239 S Michigan Ave, Chicago, IL 60616";
 });
