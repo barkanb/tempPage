a64125c89ddc334ace47be235d52dc8b1ef4cecc
Boria Barkan <42649953+barkanb@users.noreply.github.com>
2025-02-15 01:21:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const addressElement = document.getElementById("text");
-    const currentTime = new Date();
-    const targetTime = new Date();
-    
-   
-});
+
