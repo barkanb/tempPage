a9d5e3497edd655f94578557f0075b1996920fdc
Boria Barkan <42649953+barkanb@users.noreply.github.com>
2025-02-15 01:20:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,5 @@ document.addEventListener("DOMContentLoaded", function() {
     const currentTime = new Date();
     const targetTime = new Date();
     
-    // Set target time to 5 AM Central Time
-    targetTime.setHours(5, 0, 0, 0);
-    targetTime.setMinutes(targetTime.getMinutes() + targetTime.getTimezoneOffset() - 360); // Adjust for Central Time (UTC-6)
-
-    if (currentTime >= targetTime) {
-        addressElement.textContent = "Please follow us for info about the next party";
-    }
+   
 });
